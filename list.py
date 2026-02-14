835bc2434626d57c64f4f4b03dd28d0f7e769831
Sami Diriba <samediriba54@gmail.com>
2026-02-14 22:28:51 +0300

    I've learned python list

--- list.py
+++ list.py
@@ -61,3 +61,90 @@ print("After pop", a)
 
 del a[0]
 print("After del:", a)
+
+# Iterating over list
+# we can iterate over lists using loops, 
+a = ['apple', 'banana', 'cherry']
+for item in a:
+    print(item)
+
+
+# Nested list
+"""A nested list is a list within  another list, which is useful for representing
+    matrices or tables. we can access nested elements by chaining indexes"""
+matrix = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9]]
+print(matrix[1][2])
+print(matrix[2][1])
+print(matrix[0][0])
+print(matrix[2][2])
+
+#List comprehension
+""" is a concise and powerful way to create new lists by
+applying an expression to each item in an existing iterable(like a list, tuple, or range).range
+it helps you write clean, readable and efficient code compared to traditional loops"""
+#suppose you want to square every num in a list
+a = [1, 2, 4, 5]
+res = [val ** 2 for val in a]
+print(res)
+
+#Explanation:res = [val ** 2 for val in a] use list comprehension to create a new list by squaring each number in a.
+"""Syntax [expression for item in iterable if condition] 
+
+Parameters:
+    expression: operation or value to include in the new list.
+    item: current element from the iterable.
+    iterable: sequence like a list, tuple or range.
+    if condition (optional): filter to include only items that satisfy the condition.
+"""
+
+#Why use list comprehension?
+""" 
+    cleaner code:Combines looping, filterng and transformation in one line 
+    more readable:avoids verbose loops and temporary variables
+    faster execution:often faster than equivalent for-loops"""
+a = [1, 3, 5, 2]
+res = []
+for val in a:
+    res.append(val * 2)
+print(res)
+
+#Conditional statements in list comprehension
+a = [2, 3, 4, 5]
+res = [val for val in a if val%2==0] #selects only even num
+print(res)
+
+#Examples of list comprehension
+#1. Creating a list from a range
+a = [i for i in range(4)]
+print(a)
+#2.using nested loops
+c = [(x, y, z) for x in range(5) for y in range(3) for z in range(2)]
+print(c)
+#3.flattening a list of lists
+mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+res = [val for row in mat for val in row]
+print(res)
+""" Explanation:
+
+mat is a list of lists (a 3x3 matrix).
+[val for row in mat for val in row] goes through each sublist (row) and then each value (val) inside it.
+It collects all values into a single flat list"""
+
+
+#Python list comprehension using if-else
+a = [1, 4, 3, 2]
+#Add Even for even numbers otherwise Odd
+result = ['Even' if n % 2==0  else 'Odd' for n in a]
+print(result)
+
+res = [n for n in a if n%2==0]
+print(res)
+
+#Nested if-else in list comprehension
+a = [3, 4, 5, 6]
+# Categorize numbers based on divisibility
+result = ["Divisible by 2" if n % 2 == 0 else "Divisible by 3" if n % 3 == 0 else "Other" for n in a]
+print(result)
+
